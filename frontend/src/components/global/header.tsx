5a6f9103d8998d2efaeabb30c016d0c48becacd6
Aidan <aidanbh16@gmail.com>
2026-03-25 13:53:47 -0400

    Sign up page and sign in page made

--- frontend/src/components/global/header.tsx
+++ frontend/src/components/global/header.tsx
@@ -5,8 +5,8 @@ import Logo from "../../../public/logo/Lucrum Logo.png"
 
 export default function Header(){
     return(
-        <header className="w-full h-10 px-5 bg-white flex justify-center">
-            <div className="flex items-center gap-x-2">
+        <header className="max-md:hidden w-full h-10 px-5 bg-white flex justify-center fixed shadow-lg">
+            <div className="flex items-center gap-x-2 select-none">
                 <Image src={Logo} alt="Logo" className="h-9/10 w-15"/>
                 <h1 className="text-amber-400 text-2xl font-semibold text-shadow-sm">Lucrum</h1>
             </div>
